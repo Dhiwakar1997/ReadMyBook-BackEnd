bd76dcdc65ecd8ff871fccb998f08d698b21cd73
Dhiwakar <iamdhiwakar@gmail.com>
2026-01-06 19:34:18 +0530

    qdrant client setup

--- data/repositories/qdrantVecorRepository.py
+++ data/repositories/qdrantVecorRepository.py
@@ -1,10 +1,11 @@
 from qdrant_client import QdrantClient
 from qdrant_client.models import VectorParams, Distance, PointStruct
-
+import os
 
 class QdrantStorage:
-    def __init__(self, url="http://localhost:6333", collection="docs", dim=3072):
-        self.client = QdrantClient(url=url, timeout=30)
+    def __init__(self, collection="docs", dim=3072):
+        self.url = os.getenv("QDRANT_URL", "http://localhost:6333")
+        self.client = QdrantClient(url=self.url, api_key=os.getenv("QDRANT_KEY",None), timeout=30)
         self.collection = collection
         if not self.client.collection_exists(self.collection):
             self.client.create_collection(
